33c568af9e83f1903d5be3939768e687b578bbd5
Arthur <arth.msc@gmail.com>
2024-12-08 06:39:59 -0300

    Estruturando e modelando o componente small-card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { SmallCardComponent } from "../../components/small-card/small-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [MenuTitleComponent, BigCardComponent],
+  imports: [MenuTitleComponent, BigCardComponent, SmallCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
